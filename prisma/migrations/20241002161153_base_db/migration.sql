437f4a9eb397a7e52faebc3f5e9707aab4642201
xJustFox <dodydaversa@gmail.com>
2024-10-02 18:13:21 +0200

    modificato schema prisma

---
+++ prisma/migrations/20241002161153_base_db/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE `Post` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(191) NOT NULL,
+    `slug` VARCHAR(191) NOT NULL,
+    `image` VARCHAR(191) NULL,
+    `content` VARCHAR(191) NOT NULL,
+    `published` BOOLEAN NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `Post_slug_key`(`slug`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
